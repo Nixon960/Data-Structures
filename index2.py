37578134bc14cc56b0e5264d69f5f2f257e98f45
Nziriga Isaac Nixon <130220268+Nixon960@users.noreply.github.com>
2024-03-06 09:43:13 +0300

    Add files via upload
    Write a python code that implements back and forward buttons in the browser as a stack by designing a class named BrowserStack, implement the following methods: init_(), navigation(url), backward), and forward(). Use the list, pages = ["www.google.com", "www.github.com",
    "www.yahoo.com",
    "www.ucu.com",
    "www.microsoft.com",
    "www.skype.com"]
    as the
    parameters for the navigation(url) method. What is the output when the following code is run?
    browser = BrowserStacke
    print([browser. navigation(page) for page in pages])
    browser. backward browser. backward browser. forward
    browser.navigation C"www.3wschools.com")
    browser. forward)

---
+++ index2.py
@@ -0,0 +1,33 @@
+class BrowserStack:
+    def __init__(self):
+        self.stack = []
+        self.current_page = None
+    
+    def navigation(self, url):
+        if self.current_page is not None:
+            self.stack.append(self.current_page)
+            print(self.current_page)
+        self.current_page = url
+        return f"Current page: {self.current_page}"
+    
+    def backward(self):
+        if self.stack:
+            self.current_page = self.stack.pop()
+            return f"Backward to: {self.current_page}"
+        else:
+            return "No previous page to go back"
+    
+    def forward(self):
+        if self.current_page:
+            return f"Forward to: {self.current_page}"
+        else:
+            return "No forward page"
+
+pages = ["www.google.com", "www.github.com", "www.yahoo.com", "www.ucu.com", "www.microsoft.com", "www.skype.com"]
+browser = BrowserStack()
+print([browser.navigation(page) for page in pages])
+print(browser.backward())
+print(browser.backward())
+print(browser.forward())
+print(browser.navigation("www.3wschools.com"))
+print(browser.forward())
